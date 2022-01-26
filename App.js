7dc58a5d4220223ea0a19f71f119edac8d9eb67e
vxshugo <vxshugo@gmail.com>
2022-01-26 20:41:03 +0600

    Удалил в индекс файле компонент welcome и создал компонент RootNavigation для автоматизирования страниц

--- App.js
+++ App.js
@@ -1,9 +1,9 @@
 import React from "react";
 import { View } from 'react-native';
-import { Welcome } from "./screens/welcome/index"
 import style from "./screens/welcome/style";
+import RootNavigation from "./navigation/RootNavigation"
 
 export default function App() {
-  return <Welcome />;
+  return <RootNavigation />
 }
 
